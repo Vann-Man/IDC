68ce2a6d4a0c4f8a34c0483a06f37bddf55eb9fd
Vann <pok_vann_xyn@s2022.ssts.edu.sg>
2025-04-09 21:19:46 +0800

    Sandwich photos

--- cam_test.py
+++ cam_test.py
@@ -1,12 +1,14 @@
 import cv2
+import uuid
 
 cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FPS, 60)
 
 while True:
     ret, image = cam.read()
-    image = cv2.flip(image,1)
     cv2.imshow('Camera Feed', image)
+    if cv2.waitKey(1) & 0xFF == ord('t'):
+        cv2.imwrite(f"photos_sandwich/{uuid.uuid4()}.jpg", image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
