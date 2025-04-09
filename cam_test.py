e76aa87cd0feec62e7660ef2422d6c710628f397
Vann <pok_vann_xyn@s2022.ssts.edu.sg>
2025-04-09 19:34:33 +0800

    Initial commit

--- cam_test.py
+++ cam_test.py
@@ -1,8 +1,11 @@
 import cv2
 
 cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FPS, 60)
+
 while True:
     ret, image = cam.read()
+    image = cv2.flip(image,1)
     cv2.imshow('Camera Feed', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
